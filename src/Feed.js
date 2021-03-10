bd23dc1003dc866d92d085e9391d4d500796ea10
Elijah Guerrero <elijahguerrero97@gmail.com>
2021-03-09 22:38:03 -0600

    removed firebase

--- src/Feed.js
+++ src/Feed.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from "react";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
 import "./Feed.css";
-import db from "./firebase";
+// import db from "./firebase";
 import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) =>
-      setPosts(snapshot.docs.map((doc) => doc.data()))
-    );
+    // db.collection("posts").onSnapshot((snapshot) =>
+    //   setPosts(snapshot.docs.map((doc) => doc.data()))
+    // );
   }, []);
 
   return (
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@material-ui/core";
-import db from "./firebase";
+// import db from "./firebase";
 
 function TweetBox() {
   const [tweetMessage, setTweetMessage] = useState("");
@@ -10,15 +10,15 @@ function TweetBox() {
   const sendTweet = (e) => {
     e.preventDefault();
 
-    db.collection("posts").add({
-      displayName: "Rafeh Qazi",
-      username: "cleverqazi",
-      verified: true,
-      text: tweetMessage,
-      image: tweetImage,
-      avatar:
-        "https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png",
-    });
+    // db.collection("posts").add({
+    //   displayName: "Rafeh Qazi",
+    //   username: "cleverqazi",
+    //   verified: true,
+    //   text: tweetMessage,
+    //   image: tweetImage,
+    //   avatar:
+    //     "https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png",
+    // });
 
     setTweetMessage("");
     setTweetImage("");
